b5db5735c78826e833adffe395273afb4401967f
Legend <me@liux.pro>
2024-11-16 22:56:36 +0800

    big fix

--- software/backLight.h
+++ software/backLight.h
@@ -1,4 +1,7 @@
+#ifndef BACK_LIGHT_H
+#define BACK_LIGHT_H
+
 #include "common.h"
-
-
 void configBackLightPWM(u8 brightness);
+
+#endif
--- software/touch.h
+++ software/touch.h
@@ -1,3 +1,6 @@
+#ifndef TOUCH_H
+#define TOUCH_H
+
 #include "common.h"
 extern u16 X, Y;
 void touch_init();
@@ -6,3 +9,5 @@ u16 x_read();
 u16 y_read();
 u16 remap(u16 adc_value, u16 adc_min, u16 adc_max, u16 output_min, u16 output_max);
 u8 touch_scan(void);
+
+#endif
